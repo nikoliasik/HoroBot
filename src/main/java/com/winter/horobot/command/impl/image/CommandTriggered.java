55d2c41fce403d0a6f7e1925099ff1c0fb68b926
Anthony Cerruti <aprime2003@gmail.com>
2017-05-30 15:58:55 -0700

    Duplicated code, sorry. Figured I should fix this.

--- src/main/java/com/winter/horobot/command/impl/image/CommandTriggered.java
+++ src/main/java/com/winter/horobot/command/impl/image/CommandTriggered.java
@@ -68,8 +68,38 @@ public class CommandTriggered implements Command {
 			}
 		} else if(args.length == 0) {
 			IUser u = event.getAuthor();
-			for(int i = 0; i < 50; i++) {
-
+			ByteArrayOutputStream o = new ByteArrayOutputStream();
+			age.start(o);
+			age.setDelay(30);
+			age.setRepeat(0);
+			try {
+				List<BufferedImage> avatarFrames = null;
+				try {
+					URLConnection conn = new URI(u.getAvatarURL().substring(0, u.getAvatarURL().lastIndexOf('.')) + ".gif").toURL().openConnection();
+					conn.addRequestProperty("User-Agent", "Mozzila/5.0");
+					avatarFrames = Utility.getGifFramesFromStream(conn.getInputStream());
+				} catch(Exception e) {
+					URLConnection conn = new URI(u.getAvatarURL().substring(0, u.getAvatarURL().lastIndexOf('.')) + ".png").toURL().openConnection();
+					conn.addRequestProperty("User-Agent", "Mozzila/5.0");
+					avatarFrames = Collections.singletonList(ImageIO.read(conn.getInputStream()));
+				}
+				List<BufferedImage> triggeredFrames = Utility.getGifFramesFromStream(new URI("http://i.imgur.com/9WSux7S.gif").toURL().openStream());
+				for(int i = 0; i < 200; i++) {
+					BufferedImage avatarFrame = Utility.copyImage(avatarFrames.get(i % avatarFrames.size()));
+					BufferedImage triggeredFrame = triggeredFrames.get(i % triggeredFrames.size());
+					Graphics2D avatarGraphics = avatarFrame.createGraphics();
+					avatarGraphics.setColor(new Color(1.0f, 0.0f, 0.0f,i * 0.005f));
+					avatarGraphics.fillRect(0, 0, avatarFrame.getWidth(), avatarFrame.getHeight());
+					avatarGraphics.setColor(Color.WHITE);
+					avatarGraphics.drawImage(triggeredFrame, 0, 0, avatarFrame.getWidth(), avatarFrame.getHeight(), null);
+					age.addFrame(avatarFrame);
+				}
+				age.finish();
+				Message.sendFile(event.getChannel(), "", "triggered.gif", new ByteArrayInputStream(o.toByteArray()));
+			} catch (IOException e) {
+				e.printStackTrace(); // TODO correctly handle exceptions
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
 			}
 		} else {
 			Message.sendMessageInChannel(event.getChannel(), help());
